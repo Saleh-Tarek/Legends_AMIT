d2a531242fff27e98b37ae00055cb8cc35bc9dfe
Saleh-Tarek <saleh.tarek001@gmail.com>
2023-09-29 21:08:39 +0300

    Second commit

---
+++ Tootling_session/Lab1/file1.h
@@ -0,0 +1,8 @@
+#ifndef FILE1_H_
+#define FILE1_H_
+
+	void MyPrint( unsigned short int );
+
+
+
+#endif
---
+++ Tootling_session/Lab1/file2.c
@@ -0,0 +1,16 @@
+/**
+ * Auther	: Tarek Saleh
+ * Name		: Tooling Session
+ * Date		: 29/09/2023
+ * */
+	#include <stdio.h>
+	#include "file1.h"
+	int main()
+	{
+		unsigned short int Local_uint16_tVar;
+		printf("Enter an integer value.\n");
+		scanf("%hu",Local_uint16_tVar);
+		MyPrint(Local_uint16_tVar);
+	
+	}
+

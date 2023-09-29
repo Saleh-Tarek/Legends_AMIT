50318d989aa1eaea36d92ac18d74340250a39554
Saleh Tarek <124738243+Saleh-Tarek@users.noreply.github.com>
2023-09-29 21:18:04 +0300

    Update file1.c

--- Tootling_session/Lab1/file1.c
+++ Tootling_session/Lab1/file1.c
@@ -7,7 +7,7 @@
 	#include <stdio.h>
 	void MyPrint( unsigned short int Copy_uint16_tVar )
 	{
-		printf("The output is:\n\t%hu",Copy_uint16_tVar);
+		printf("The output is:\n\t%hu\n",Copy_uint16_tVar);
 
 	
 	}
